cd6bf98a92ec46df0713969b78a9cbf51b7c68e6
Hariharan2716 <hariharan.m2716@gmail.com>
2025-05-08 12:01:57 +0530

    Added POST endpoint for creating a new skill

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const skillRoutes = require('./routes/skillRoutes');
 const app = express();
 
+app.use(express.json());  // added this Middleware to parse JSON in post-api
 app.use('/api',skillRoutes);
 
 
