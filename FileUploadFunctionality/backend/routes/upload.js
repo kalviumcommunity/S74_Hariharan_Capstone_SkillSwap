583bc86b5d1acb44ae96fda04b7697041affe3f9
Hariharan2716 <hariharan.m2716@gmail.com>
2025-05-23 10:42:23 +0530

    Added Upload functionality in the application

---
+++ FileUploadFunctionality/backend/routes/upload.js
@@ -0,0 +1,30 @@
+// backend/routes/upload.js
+
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+const path = require('path');
+
+// Storage config
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer ({storage: storage});
+
+// upload endpoint
+router.post('/', upload.single('file'), (req, res) => {
+  if(!req.file) return res.status(400).json({error: 'No file uploaded'});
+
+  res.json({
+    message: 'File uploaded successfully',
+    filePath: `/uploads/${req.file.filename}`,
+  });
+});
+
+module.exports = router;
---
+++ FileUploadFunctionality/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// frontend/src/App.jsx
+
+import FileUpload from "./components/FileUpload";
+
+function App() {
+  return (
+    <div>
+      <h1>SkillSwap Upload Demo</h1>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default App;
